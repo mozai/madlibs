8a1d23686f6887230745920ee1ee8619e042ad4d
Moses Moore <moses-github@mozai.com>
2016-05-10 01:38:45 -0400

    Changing now '@' is done; and PEP8 conformance.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Madlibs module for Python
 =========================
 
-The idea: class that you instantiate with a vocabulary dict(). Ask 
-the instance for a random story, it selects a random story-template 
-from its vocabulary, substituting marked spots with appropriate 
+The idea: class that you instantiate with a vocabulary dict(). Ask
+the instance for a random story, it selects a random story-template
+from its vocabulary, substituting marked spots with appropriate
 random words from elsewhere in its vocabulary.
 
 
@@ -11,14 +11,15 @@ Example Vocabulary
 ------------------
     "It sure is %(sunny|wet|hot|cold|awful) here in %city."
 
-The first marked area `%(sunny|wet|hot|cold|awful)` will choose one 
-of the items seperated by '|' marks.  The second marked area `%city` 
+The first marked area `%(sunny|wet|hot|cold|awful)` will choose one
+of the items seperated by '|' marks.  The second marked area `%city`
 will use a 'city' list defined elsewhere in the vocabulary.
 
 Here is an example vocabulary dict():
 
     {
-      "@": ["weather"],
+      "#": "This term is not used it is only comments",
+      "@": ["%weather"],
       "city": [ "Cincinati", "Montreal", "London" ]
       "weather": [
         "It sure is %(sunny|wet|hot|cold|awful) here in %city.",
@@ -27,8 +28,8 @@ Here is an example vocabulary dict():
       ]
     }
 
-The names of the lists the vocabulary are called *terms*.  In the 
-example above, `%city` marks an area that will be replaced with a 
+The names of the lists the vocabulary are called *terms*.  In the
+example above, `%city` marks an area that will be replaced with a
 random item from the 'city' list.
 
 Here is example code that would use the above vocabulary:
@@ -43,32 +44,32 @@ Terms and Values
 Terms and Values correspond to the key/value pairs of a dict(),
 with some extra rules for how they are to be used.
 
-**Terms** in the vocabulary must start with an alpha char (A-Za-z) 
+**Terms** in the vocabulary must start with an alpha char (A-Za-z)
 and have only characters in the set (A-Za-z0-9.\_)  *('.' will have a
-special meaning later, but it will be backwards compatible)*  Terms 
-that are a single character from the set (!@#$%^&\*) are reserved 
-for internal use.  
+special meaning later, but it will be backwards compatible)*  Terms
+that are a single character from the set (!@#$%^&\*) are reserved
+for internal use.
 
 Of the reserved Terms, two are currently used:
 
-- The '@' Term is used for identifying Term used to start stories, so 
-  that a vocabulary can have many stories and can be asked for a 
-  random one, instead of being provided a story string to mix up.
-  Each vocabulary **must** have a valid '@' Term. 
-- The '#' Term is used for comments about the vocabulary itself, 
+- The '@' Term is used for starting stories, either one string
+  or a list of strings, that are the default if you don't pass
+  a parameter to .story().
+  Each vocabulary **must** have a valid '@' Term.
+- The '#' Term is used for comments about the vocabulary itself,
   such as author, citing sources, intended use, and so on.  A
   vocabulary doesn't need to have a '#' Term, but it's recommended.
 
-**Values** are the items in a term's list.  Values are strings or 
-unicode strings.  When a term is requested from a madlibs object 
-exactly one value will be returned.  If a Term has more than one 
+**Values** are the items in a term's list.  Values are strings or
+unicode strings.  When a term is requested from a madlibs object
+exactly one value will be returned.  If a Term has more than one
 Value, all possible Values will be used before repeating.
 
-Values may also contain %(a|b|c) or %abc marked areas; 
-the former will be a choice of replacedments for that area, the 
-latter will be checked to see if it is a Term, and replaced with a 
-value from that Term, otherwise left alone. A value from a such a 
-Term may contain other markers, bringing in other values; loops are 
+Values may also contain %(a|b|c) or %abc marked areas;
+the former will be a choice of replacedments for that area, the
+latter will be checked to see if it is a Term, and replaced with a
+value from that Term, otherwise left alone. A value from a such a
+Term may contain other markers, bringing in other values; loops are
 possible, but if a loop goes on too often an exception will be raised.
 
 
@@ -88,9 +89,9 @@ or a file-like object:
     ML = madlibs.Madlibs
     ML.load(handmade_dict)
 
-The current version expects the file to be a valid JSON dict(). You 
-can also provide any other dict() object that obeys the rules for a 
-valid vocabulary, which permits use of the Python module 'shelve' 
+The current version expects the file to be a valid JSON dict(). You
+can also provide any other dict() object that obeys the rules for a
+valid vocabulary, which permits use of the Python module 'shelve'
 or other methods for persistence.
 
 
--- madlibs.py
+++ madlibs.py
@@ -1,18 +1,21 @@
 # madlibs.py
 # encoding: utf8
 # author: Mozai <moc.iazom@sesom>
-# version: 20121213
+# version: 20160509
 """ Madlibs takes a dict as vocabulary of story-strings with marked spots
 for substituting random words or phrases from collections.   It is
 intended to be used iteratively, and uses a randomizing method
 intended for high novelty in the output.
 God this description is poor; look for a readme.txt with this.  Especially
-for how to make   the vocabulary data-sets used to initialize this.
+for how to make the vocabulary data-sets used to initialize this.
 """
-import json, random, shelve
-import anydbm # only so I can catch anydbm.error
+import anydbm  # only so I can catch anydbm.error
+import json
+import random
+import shelve
 
-# TODO: if 'ab':['%cd'], then '%ab' -> '%cd' -> 'CD', but '%{ab}s' -> '%cds' -> error
+# TODO: if 'ab':['%cd'], then '%ab' -> '%cd' -> 'CD',
+#       but '%{ab}s' -> '%cds' -> error
 # wanted: backref "An %bodyPart for an %{\1} makes the whole world blind."
 # test: 'alpha.bet' is a valid term, decays to 'alpha' if 'alpha.bet' empty.
 # thinking: Would it be better to subclass UserDict ?
@@ -21,6 +24,7 @@ import anydbm # only so I can catch anydbm.error
 BRACEPAIRS = (('(', ')'), ('[', ']'), ('{', '}'), ('<', '>'), ('|', '|'))
 TERMCHARS = '.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
 
+
 def _next_term_ij(data, start=0):
   # find %blah or %(blah) in string data; returns (i,j) pair for s[i:j]"
   # used repeatedly in Madlibs.get()
@@ -28,23 +32,24 @@ def _next_term_ij(data, start=0):
   j = -1
   while i < 0 and j < 0:
     i = data.find('%', start)
-    if (i+1 == len(data) or i < 0):
+    if ((i + 1) == len(data)) or (i < 0):
       i = -1
       break
     for bracepair in BRACEPAIRS:
-      if data[i+1] == bracepair[0]:
-        j = data.find(bracepair[1], i+2) + 1
-    if j < i :
-      if i+2 < len(data) and data[i:i+2] == '%%':
-        return (i, i+2)
-      j = i+1
-      while j < len(data) and (data[j] in TERMCHARS) :
+      if data[i + 1] == bracepair[0]:
+        j = data.find(bracepair[1], i + 2) + 1
+    if j < i:
+      if ((i + 2) < len(data)) and (data[i:i + 2] == '%%'):
+        return (i, i + 2)
+      j = i + 1
+      while j < len(data) and (data[j] in TERMCHARS):
         j += 1
-    if j == i+1:
-      start = i+1
+    if j == i + 1:
+      start = i + 1
       i = -1
       j = -1
   return (i, j)
+
 
 def _extract_terms(data):
   # "%alpha is a %beta with a huge %gamma" -> ['alpha', 'beta', 'gamma']
@@ -52,12 +57,13 @@ def _extract_terms(data):
   (i, j) = (0, 0)
   (i, j) = _next_term_ij(data)
   while (i, j) != (-1, -1):
-    term = data[i+1:j]
-    if (term[0], term[-1]) in BRACEPAIRS :
+    term = data[i + 1:j]
+    if (term[0], term[-1]) in BRACEPAIRS:
       term = term[1:-1]
     terms.append(term)
     (i, j) = _next_term_ij(data, j)
   return terms
+
 
 def _good_term(termname):
   # validate a term name
@@ -66,7 +72,7 @@ def _good_term(termname):
     return False
   if not termname[0].isalpha():
     return False
-  if not termname.replace('.','').replace('_','').isalnum() :
+  if not termname.replace('.', '').replace('_', '').isalnum():
     return False
   return True
 
@@ -79,16 +85,16 @@ class Madlibs(object):
   Be ready to catch madlibs.VocabularyError or TypeError or KeyError
   """
 
-  def __init__(self, vocabulary=None) :
+  def __init__(self, vocabulary=None):
     " can pass a dict for vocabulary or filename to a vocabulary file "
-    self.vocabulary = { '#':'', '@':[] }
+    self.vocabulary = {'#': '', '@': ''}
     self.shuffles = {}
     self.errors = []
     self.warnings = []
     self.__cmp__ = self.vocabulary.__cmp__
     self.__iter__ = self.vocabulary.__iter__
     self.__contains__ = self.vocabulary.__contains__
-    if vocabulary :
+    if vocabulary:
       self.load(vocabulary)
 
   def __del__(self):
@@ -110,24 +116,24 @@ class Madlibs(object):
     """
     if not isinstance(self.vocabulary, (dict, shelve.Shelf)):
       raise VocabularyError('vocabulary bad data type "%s"' % type(self.vocabulary))
-    if not '@' in self.vocabulary :
+    if '@' not in self.vocabulary:
       raise VocabularyError('term "@" is missing')
-    for term in self.vocabulary.keys() :
-      if term == '@' :
-        if not isinstance(self.vocabulary['@'], list) :
-          raise VocabularyError('term "@" is not a list')
-        elif len(self.vocabulary['@']) == 0 :
+    for term in self.vocabulary.keys():
+      if not isinstance(self.vocabulary[term], (basestring, list)):
+        raise VocabularyError('term %s is type %s' % (term, type(self.vocabulary[term])))
+      if term == '@':
+        if len(self.vocabulary['@']) == 0:
           raise VocabularyError('term "@" is empty')
-      elif term == '#' :
-        if not isinstance(self.vocabulary.get('#',''), basestring):
+      elif term == '#':
+        if not isinstance(self.vocabulary.get('#', ''), basestring):
           raise VocabularyError('term "#" is not a string')
-      elif not _good_term(term) :
+      elif not _good_term(term):
         raise VocabularyError('term "%s" is illegal' % term)
     return True
 
   def delterm(self, term):
     " for removing a term from the vocabulary "
-    del(self.vocabulary[term])
+    del self.vocabulary[term]
 
   def __delitem__(self, term):
     " see delterm(t) "
@@ -142,14 +148,14 @@ class Madlibs(object):
     self.vocabulary = None
     if isinstance(filenameordict, (dict, shelve.Shelf)):
       self.vocabulary = filenameordict
-    elif isinstance(filenameordict, basestring) :
+    elif isinstance(filenameordict, basestring):
       try:
         self.vocabulary = shelve.open(filenameordict, flag='w')
-      except anydbm.error:
-        infile = open(filenameordict,'r')
+      except anydbm.error:  # not my fault this doesn't inherit from BaseException
+        infile = open(filenameordict, 'r')
         self.vocabulary = json.load(infile)
         infile.close()
-    if self.vocabulary :
+    if self.vocabulary:
       self.__iter__ = self.vocabulary.__iter__
       self.__contains__ = self.vocabulary.__contains__
     return self._is_valid()
@@ -179,7 +185,7 @@ class Madlibs(object):
         json.dump(self.vocabulary, filething, sort_keys=True, indent=2)
       filething.close()
     else:
-      raise TypeError("expected str or file object; received "+ type(filething))
+      raise TypeError("expected str or file object; received " + type(filething))
 
   def validate(self):
     """ checks the vocabulary for problems
@@ -187,8 +193,8 @@ class Madlibs(object):
     and populates self.errors and self.warnings as a list
     """
     # "too many branches (20/12)"  shut up, pylint
-    self.errors = []   # side effects! hisssss!
-    self.warnings = [] # side effects! hisssss!
+    self.errors = []    # side effects! hisssss!
+    self.warnings = []  # side effects! hisssss!
     try:
       self._is_valid()
     except VocabularyError as err:
@@ -196,28 +202,25 @@ class Madlibs(object):
     terms = self.vocabulary.keys()
     terms.sort()
     termsseen = set()
+    if isinstance(self.vocabulary['@'], basestring):
+      first_stories = [self.vocabulary['@']]
+    else:
+      first_stories = self.vocabulary['@']
+    for story in first_stories:
+      if not _extract_terms(story):
+        self.warnings.append("no substitutions in '@' \"%s\"" % story)
     for term in terms:
       try:
         if term in '!$%^&*':
           # reserved, presently unused
           continue
-        elif term == '@':
-          for story in self.vocabulary['@']:
-            termsseen.add(story)
-            if not _good_term(story):
-              self.errors.append('bad story Term \"%s\" in \'@\'' % story)
-            if self.vocabulary.get(story) is None:
-              self.errors.append('missing story Term \"%s\" in \'@\'' % story)
-            for storyterm in self.vocabulary[story]:
-              if not _extract_terms(storyterm) :
-                self.warnings.append("no substitutions in \"%s\"" % storyterm)
         elif term == '#':
           # already checked with self._is_valid()
           pass
-        else :
+        else:
           values = self.vocabulary[term]
           if isinstance(values, basestring):
-            values = [values,]
+            values = [values, ]
           if not isinstance(values, list):
             self.errors.append('term %s has unexpected value type "%s"' % (term, type(values)))
           elif len(values) == 0:
@@ -237,30 +240,30 @@ class Madlibs(object):
       except (VocabularyError, StandardError) as err:
         self.errors.append('term "%s": %s' % (term, repr(err)))
     for term in terms:
-      if len(term) > 1 and not term in termsseen :
+      if len(term) > 1 and term not in termsseen:
         self.warnings.append('term "%s" is unused' % term)
     return len(self.errors) == 0
 
   def addterm(self, term, data=None):
     " for adding a new term to the vocabulary "
-    if term in self :
+    if term in self:
       self.insert(term, data)
     elif not _good_term(term):
       raise TypeError('bad term; "%s"' % type(term))
-    elif data == None:
+    elif data is None:
       self.vocabulary[term] = []
     elif isinstance(data, basestring):
       self.vocabulary[term] = [data]
     elif isinstance(data, list):
       self.vocabulary[term] = list(data)
     else:
-      raise TypeError('bad data; expected str or list; received '+ type(data))
+      raise TypeError('bad data; expected str or list; received ' + type(data))
 
   def insert(self, term, data=None):
     " for adding more values to a term in the vocabulary "
     if not isinstance(term, basestring):
-      raise TypeError('bad term; expected str, got '+ type(term))
-    if not term in self.vocabulary:
+      raise TypeError('bad term; expected str, got ' + type(term))
+    if term not in self.vocabulary:
       self.addterm(term, data)
     elif isinstance(data, basestring):
       self.vocabulary[term].append(data)
@@ -271,7 +274,7 @@ class Madlibs(object):
         temp.append(i)
       self.vocabulary[term] = temp
     else:
-      raise TypeError('bad data; expected str or list, got '+ type(data))
+      raise TypeError('bad data; expected str or list, got ' + type(data))
 
   def __setitem__(self, term, data):
     "see insert()"
@@ -282,8 +285,8 @@ class Madlibs(object):
     The entire item must be included with marks intact and case-sensitive
     If the item isn't associated with this term, it will return False.
     """
-    if not term in self.vocabulary:
-      raise KeyError('term not found in vocabulary; '+ term)
+    if term not in self.vocabulary:
+      raise KeyError('term not found in vocabulary; ' + term)
     try:
       self.vocabulary[term].remove(data)
     except ValueError:
@@ -294,20 +297,13 @@ class Madlibs(object):
     """how many random stories are available in the loaded vocabulary.
     This is not the same as how many terms are in the vocabulary.
     """
-    leng = 0
-    if len(self.vocabulary) == 0 :
+    if len(self.vocabulary) == 0:
       return None
-    if not '@' in self.vocabulary :
-      return leng
-    for i in self.vocabulary['@']:
-      try:
-        if isinstance(self.vocabulary[i], basestring):
-          leng += 1
-        else:
-          leng += len(self.vocabulary[i])
-      except KeyError:
-        leng += 0
-    return leng
+    if '@' not in self.vocabulary:
+      return 0
+    if isinstance(self.vocabulary['@'], basestring):
+      return 1
+    return len(self.vocabulary['@'])
 
   def __getitem__(self, term):
     "see get()"
@@ -320,11 +316,11 @@ class Madlibs(object):
     single-character term is not allowed; will raise KeyError
     """
     if not isinstance(term, basestring):
-      raise TypeError('bad term type '+repr(type(term)))
+      raise TypeError('bad term type ' + repr(type(term)))
     if term[0] == '%':
       # in case we passed %fruit instead of 'fruit'
       term = term[1:]
-    if not term :
+    if not term:
       # we were passed '%' or ''; eye-dee-ten-tee issue
       return default
     if (term[0], term[-1]) in BRACEPAIRS:
@@ -337,15 +333,15 @@ class Madlibs(object):
       # else it's %fruitcolour type
       key = term
       values = self.vocabulary.get(key)
-      while (values == None and '.' in key) :
+      while values is None and '.' in key:
         key = key[:key.rindex('.')]
         values = self.vocabulary.get(key)
-      if isinstance (values, basestring):
-        values = [values,]
+      if isinstance(values, basestring):
+        values = [values, ]
 
     value = default
-    if values :
-      if not self.shuffles.get(term) :
+    if values:
+      if not self.shuffles.get(term):
         self.shuffles[term] = list(range(len(values)))
         random.shuffle(self.shuffles[term])
       value = values[self.shuffles[term].pop()]
@@ -354,94 +350,97 @@ class Madlibs(object):
 
   def comments(self, data=None):
     " get/set comment text for the vocabulary "
-    if data == None:
+    if data is None:
       return self.vocabulary.get('#')
     else:
       self.vocabulary['#'] = str(data)
 
-  def templates(self, termlist=None):
-    """ get/set which terms in the vocabulary are story templates
-    terms provided must already exist in the vocabulary
-    """
-    if termlist == None :
-      return self.vocabulary['@']
-    elif isinstance(termlist, basestring):
-      if not termlist in self.vocabulary:
-        raise KeyError('bad template list; "'+termlist+'" not found in vocabulary')
-      self.vocabulary['@'] = [termlist,]
-    elif isinstance(termlist, list):
-      for i in termlist:
-        if not i in self.vocabulary:
-          raise KeyError('bad template list; "'+i+'" not found in vocabulary')
-      self.vocabulary['@'] = termlist
-    else:
-      raise ValueError('improper type "%s" for termlist' % type(termlist))
-
-  def get_template(self, term=None, search=None):
-    """ fetches a random template from the vocabulary.
-    term: use this term; can be a list of terms
-    search: only choose from the stories that have this substring
-    """
-    if self.vocabulary == None :
-      return None
-    if not '@' in self.vocabulary :
-      raise VocabularyError("missing '@' term in vocabulary")
-    if isinstance(term, basestring) :
-      template_types = [term,]
-    elif isinstance(term, list) :
-      template_types = list(term)
-    else:
-      template_types = self.vocabulary['@']
-    if search :
-      stories = []
-      for i in template_types:
-        for j in self.vocabulary[i]:
-          if j.find(search) >= 0 :
-            stories.append(j)
-      if len(stories) == 0:
-        # no matches among the template_types we're supposed to use
-        return None
-      else:
-        return random.choice(stories)
-    else:
-      return self.get(random.choice(template_types))
+#  def templates(self, termlist=None):
+#    """ get/set which terms in the vocabulary are story templates
+#    terms provided must already exist in the vocabulary
+#    """
+#    if termlist == None :
+#      return self.vocabulary['@']
+#    elif isinstance(termlist, basestring):
+#      if not termlist in self.vocabulary:
+#        raise KeyError('bad template list; "'+termlist+'" not found in vocabulary')
+#      self.vocabulary['@'] = [termlist,]
+#    elif isinstance(termlist, list):
+#      for i in termlist:
+#        if not i in self.vocabulary:
+#          raise KeyError('bad template list; "'+i+'" not found in vocabulary')
+#      self.vocabulary['@'] = termlist
+#    else:
+#      raise ValueError('improper type "%s" for termlist' % type(termlist))
+#
+#  def get_template(self, term=None, search=None):
+#    """ fetches a random template from the vocabulary.
+#    term: use this term; can be a list of terms
+#    search: only choose from the stories that have this substring
+#    """
+#    if self.vocabulary == None :
+#      return None
+#    if not '@' in self.vocabulary :
+#      raise VocabularyError("missing '@' term in vocabulary")
+#    if isinstance(term, basestring) :
+#      template_types = [term,]
+#    elif isinstance(term, list) :
+#      template_types = list(term)
+#    else:
+#      template_types = self.vocabulary['@']
+#    if search :
+#      stories = []
+#      for i in template_types:
+#        for j in self.vocabulary[i]:
+#          if j.find(search) >= 0 :
+#            stories.append(j)
+#      if len(stories) == 0:
+#        # no matches among the template_types we're supposed to use
+#        return None
+#      else:
+#        return random.choice(stories)
+#    else:
+#      return self.get(random.choice(template_types))
 
   def story(self, template=None):
     """ in template, replaces all the %(word1|word2|word3) with a
     random item and replaces %keyname with a random item from the
     vocab if template not provided, gets a new one from get_template()
     """
-    if template == None or template == '@':
-      template = self.get_template()
-    if template == None:
-      return None
+    # if template == None or template == '@':
+    #   template = self.get_template()
+    # if template == None:
+    #   return None
+    if template is None:
+      template = self.get('@')
     text = template
     bad_loop = []
     (i, j) = _next_term_ij(text, 0)
     old_j = -1
-    while (i >= 0 and j > 0):
-      if text[i:j] == '%%' :
-        text = text[:i] + '%' + text[i+2:]
-        (i, j) = _next_term_ij(text, i+3)
+    while i >= 0 and j > 0:
+      if text[i:j] == '%%':
+        text = text[:i] + '%' + text[i + 2:]
+        (i, j) = _next_term_ij(text, i + 3)
         continue
-      vocabword = text[i+1:j] # doesn't pick up the %
+      vocabword = text[i + 1:j]  # doesn't pick up the %
       try:
         vocabword = self.get(vocabword)
       except KeyError as err:
-        raise VocabularyError('bad template "%s"; (%s)' % (template, str(err)) )
-      if (vocabword != None):
+        raise VocabularyError('bad template "%s"; (%s)' % (template, str(err)))
+      if vocabword is not None:
         text = text[:i] + vocabword + text[j:]
         (i, j) = _next_term_ij(text, i)
       else:
-        (i, j) = _next_term_ij(text, i+1)
-      if j > 0 and j <= old_j :
+        (i, j) = _next_term_ij(text, i + 1)
+      if j > 0 and j <= old_j:
         bad_loop.append(vocabword)
         if len(bad_loop) > 5:
-          raise VocabularyError('vocabulary loop detected '+ bad_loop )
+          raise VocabularyError('vocabulary loop detected ' + bad_loop)
       else:
         bad_loop = []
       old_j = j
     return text
+
 
 class VocabularyError(Exception):
   """ For errors caused by misunderstanding the internal structure
